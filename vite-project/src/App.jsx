c1d8ec92226ddf90b20eb6e027d7279c93d86b89
Cameron Rogers <cameronrogers@Camerons-Laptop.local>
2025-05-14 16:52:26 -0400

    Initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css';   
+import Header from './componets/Header.jsx' // Make sure these exist
+
+function App() {
+  return (
+   <>
+   <Header/>
+   
+   </>
+  );
+}
+
+export default App;
