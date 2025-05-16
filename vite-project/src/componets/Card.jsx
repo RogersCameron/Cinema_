b86859040ceb406f0748b3563ece3f6f8584b843
Cameron Rogers <cameronrogers@Camerons-Laptop.local>
2025-05-16 11:54:39 -0400

    Working on Card componets

--- vite-project/src/componets/Card.jsx
+++ vite-project/src/componets/Card.jsx
@@ -1,10 +1,30 @@
-const Card = () => {
+const Card = ({
+  title = "this",
+  image = "/placeholder.svg",
+  rating = "8",
+  genres = ["Action", "Drama",],
+}) => {
   return (
     <>
-      <div className="flex flex-row gap-5">
-        <div className="border w-60 h-80 rounded-xl ">
-            <div><h1></h1></div>
+      <div className=" mt-5 relative border w-60 h-80 rounded-xl bg-white">
+        <img
+          src={image}
+          alt={title}
+          className="w-full h-full border rounded-xl border-black "
+        />
+        <div className=" absolute bottom-0 left-0 p-2 w-full ">
+          <h1>{title}</h1>
+          <h1>{rating}</h1>
+          <div className="flex flex-row gap-2 flex-wrap text-sm">
+          {genres.map((genre, index) =>( <span key={index} className="bg-slate-700 px-2 py-1 rounded">{genre}
 
+          </span>))}
+          </div>
+        </div>
+        <div className="absolute top-0 right-0 p-2 ">
+          <button className="bg-white text-black border border-gray-300 rounded-full w-8 h-8 text-xl shadow-md hover:bg-gray-100 transition">
+            +
+          </button>
         </div>
       </div>
     </>
--- vite-project/src/componets/MainContent.jsx
+++ vite-project/src/componets/MainContent.jsx
@@ -1,11 +1,12 @@
-import Card from "./Card"
-const MainContent = () =>{
-    return(<>
-    <Card />
-    
+import Card from "./Card";
+const MainContent = () => {
+  return (
+    <>
+      <div className="flex flex-row bg-slate-600 ">
+        <Card />
+      </div>
+    </>
+  );
+};
 
-    </>)
-
-}
-
-export default MainContent 
+export default MainContent;
